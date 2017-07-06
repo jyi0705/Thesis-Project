465f28f6bf2500c2e74053c136c688853e844e55
William Wang <will.wang8421@gmail.comgit config --global user.email will.wang8421@gmail.com>
2017-07-06 14:57:42 -0700

    deleted extra files

---
+++ public/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+    super();
+
+  }
+
+  render(){
+    return(
+      <div>HI</div>
+    )
+  }
+}
+
+export default App;
--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 
