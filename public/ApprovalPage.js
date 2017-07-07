8a7b3b52b846ee3b91ecb091f9dc6dc9b7701c88
William Wang <will.wang8421@gmail.comgit config --global user.email will.wang8421@gmail.com>
2017-07-06 18:24:49 -0700

    approvalpage add instructions

--- public/ApprovalPage.js
+++ public/ApprovalPage.js
@@ -76,7 +76,11 @@ class ApprovalPage extends Component {
         <br />
         <br />
           <b>INSTRUCTIONS TO SEND IN MOUTH SWAB:</b>
-
+          <ol>
+            <li>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation </li>
+            <li>ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum </li>
+            <li>dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</li>
+          </ol>
       </div>
     )
   }
