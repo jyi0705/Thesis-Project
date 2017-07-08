3216f6f66311314714edfa18365f1c99a55702f9
Jonathan <jyi1991@gmail.com>
2017-07-08 14:49:20 -0700

    Mongo database set up with routes and controllers

---
+++ database/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  walletId: { type: String, unique: true },
+  email: String,
+  startAge: { type: Number, min: 20 },
+  verified: { type: Boolean, default: false },
+  testResults: {type: [{isLiving: Boolean, date: Date, age: Number}], default: [{ isLiving: false, date: null, age: null}]}
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User 
--- server/server.js
+++ server/server.js
@@ -2,13 +2,28 @@ const express = require('express');
 const parser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
+const mongoose = require('mongoose')
+const userRoutes = require('./userRoutes')
+const adminRoutes = require('./adminRoutes')
+const env = process.env.NODE_ENV || 'development'
+const config = require('../database/config').config[env]
+
+mongoose.connect(config.url)
+
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+  console.log('Connected to DB')
+})
 
 const app = express()
   .use(parser.json())
   .use(parser.urlencoded({ extended: true }))
   .use(express.static('/public'))
   .use(morgan('dev'))
-
+  .use('/api/users', userRoutes)
+  .use('/api/admin', adminRoutes)
 
 app.listen(PORT, err => {
   if(err){
