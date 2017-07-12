1267d7ae3424388f6ff96b483055b8dfb3af93b6
William Wang <will.wang8421@gmail.comgit config --global user.email will.wang8421@gmail.com>
2017-07-12 11:46:15 -0700

    changes to the frontend

---
+++ public/Containers/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { HashRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import ReduxPromise from 'redux-promise';
+import reducers from '../Reducers/RootReducer';
+import Landing from './Components/LandingPage.jsx';
+import ApprovalPage from './Containers/ApprovalPage.jsx';
+import Contributing from './Components/Contributing.jsx';
+import About from './Components/About.jsx';
+import FAQ from './Components/FAQ.jsx';
+import TokenDetail from './Components/TokenDetail.jsx';
+import Admin from './src/Admin/Admin.jsx';
+
+const store = createStore(reducers, applyMiddleware(ReduxThunk, ReduxPromise));
+
+// import { Route, HashRouter, NavLink } from 'react-router-dom';
+
+
+
+const Routes = () => {
+ return (
+   <HashRouter>
+    <div className="container">
+
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Ethereum Token</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><NavLink exact to="/about" exact activeClassName="activeNav">About</NavLink></li>
+              <li><NavLink to="/contributing" activeClassName="activeNav">Contributing</NavLink></li>
+              <li><NavLink to="/tokenDetail" activeClassName="activeNav">Token Details</NavLink></li>
+              <li><NavLink to="/approval" activeClassName="activeNav">Get Approved</NavLink></li>
+              <li><NavLink to="/faq" activeClassName="activeNav">FAQ</NavLink></li>
+              <li className="nav-item">
+                <a href="https://twitter.com/" target="_blank" className="nav-link">
+                    <i className="fa fa-twitter" aria-hidden="true"></i> Link to our twitter(twitter icon)
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="https://github.com/" target="_blank" className="nav-link"> Link to our github(github icon)
+                    <i className="fa fa-github" aria-hidden="true"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/approval" component={ApprovalPage}/>
+        <Route path="/contributing" component={Contributing}/>
+        <Route path="/tokenDetail" component={TokenDetail}/>
+        <Route path="/faq" component={FAQ}/>
+        <Route path="/about" component={About}/>
+        <Route path="/admin" component={Admin}/>
+    </div> 
+   </HashRouter>
+ ) 
+}
+
+export default Routes;
--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './src/App.jsx';
-import Routes from './routes';
+// import Routes from './routes';
 
 
-ReactDOM.render(<Routes />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
