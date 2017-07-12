b55b4d81457921bbb29e52a1fea7c4f7206ffe97
Jonathan <jyi1991@gmail.com>
2017-07-12 12:43:38 -0700

    redux

--- public/Components/Contributing.jsx
+++ public/Components/Contributing.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Contributing extends Component {
   render() {
+      console.log('you are in contributing')
     return (
   <div className="scroll-point" id="contributing">
       <section className="contributing py-4 bg-white">
