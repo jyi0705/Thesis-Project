ef9f4c4e23912724e9b6625bb34c571f02199b2f
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-07-15 08:55:08 -0700

    Starts to add comments to contract functions

--- contract/contracts/Instrument.sol
+++ contract/contracts/Instrument.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.4;
 import "./IterableMapping.sol";
 import "./UtilsLib.sol";
 
+/** @title Gennuity: EthTech for social security */
 contract Instrument {
 
   /* Contract-specific structures */
@@ -21,7 +22,7 @@ contract Instrument {
 
   /* Local variables */
   Pool[] pools;
-  mapping(address => Participant) private waitlist;
+  mapping(address => Participant) private verifiedUsers;
   mapping(address => uint) public pendingDividends;
   bool private stopped = false;
   address private owner;
@@ -58,12 +59,9 @@ contract Instrument {
     require(msg.sender == owner);
     _;
   }
-  // modifier checkInvariants {
-
-  // }
 
   /**
-
+   * @dev Creates the Gennuity contract.
    */
   function Instrument() {
     uint BASE = 20;
@@ -74,21 +72,27 @@ contract Instrument {
     }
   }
   
+  /**
+   * @dev Gives information about a specific pool.
+   * @param idx Index for the pool of interest.
+   * @return participants Number of users in the pool.
+   * @return totalEth Collective amount of 
+   * @return midAge
+   */
   function pool(uint idx) public returns (uint participants, uint totalEth, uint midAge) {
     participants = pools[idx].participants.size;
     totalEth = pools[idx].totalEth;
     midAge = pools[idx].midAge;
   }
-  /**
 
-   */  
+  /**
+   * @dev Gives information about a specific pool.
+   * @param idx Index for the pool of interest.
+   * @return participants Number of users in the pool.
+   * @return totalEth Collective amount of 
+   * @return midAge
+   */
   function createPool(uint midAge) private {
-    // TODO : create a new pool for the collection
-    // pools.push(Pool({
-    //   participants: participants.set(this, 0, address),
-    //   total: 1.
-    //   midAge: midAge
-    // }))
     Pool storage newPool;
     newPool.midAge = midAge;
     newPool.totalEth = 0;
@@ -97,78 +101,42 @@ contract Instrument {
   }
   
   /**
-
+   * @dev Admin verifies the age of the user after genomic test. Allows user
+   *   to sign up for the contract.
+   * @param addr Address to be verified.
+   * @param age Age of the verified user. Reference for adding to pool, and 
+   *   also allowing them to sign up only within one year. 
    */
   function verify(address addr, uint age) public {
-    waitlist[addr].verified = true;
-    waitlist[addr].startAge = age;
-    waitlist[addr].added = false;
-    waitlist[addr].approvalDate = block.timestamp;
+    verifiedUsers[addr].verified = true;
+    verifiedUsers[addr].startAge = age;
+    verifiedUsers[addr].added = false;
+    verifiedUsers[addr].approvalDate = block.timestamp;
     LogEvent(addr, age, "verified user");
   }
 
+  /**
+   * @dev Fallback function to recieve payments. Prevents any "donations"
+   *   being sent to the contract because this a) costs gas, b) requires 10eth.
+   */
   function () payable {
     uint COST = 10 * (10 ** 18);
-    Participant user = waitlist[msg.sender];
-
-    // if (!user.verified) {
-    //   LogEvent(msg.sender, user.startAge, "sender is not verified");
-    //   throw;
-    // }
-
-    // if (user.added) {
-    //   LogEvent(msg.sender, user.startAge, "sender is already participating");
-    //   throw;
-    // } 
-
-    // if(msg.value < COST) {
-    //   LogEvent(msg.sender, user.startAge, "did not meet buy-in criteria");
-    //   throw;
-    // }
+    Participant user = verifiedUsers[msg.sender];
 
     assert(!user.added);
     assert(user.verified);
     assert(msg.value >= COST);
 
     signContract(user);
-  // function () public payable stopInEmergency {
-  //   LogEvent(msg.sender, 0, "Donation or incorrect payment made to fallback function.");
   }
-   /**
 
+  /**
+   * @dev Adds a participant to the correct pool after a verified 
+   *   user sends ether to invoke contract.
+   * @param user The cleared user to be added.
    */
   function signContract(Participant user) private {
-    // uint index = poolForAge(user).participants.size;
-    // poolForAge(user).participants.set(this, index, user);
-    // uint COST = 50 ** 18;
-
-  // function signContract() public payable stopInEmergency {
-    // uint index = poolForAge(user).participants.size;
-    // poolForAge(user).participants.set(this, index, user);
-    // uint COST = 50 ** 18;
     uint COST = 10 * (10 ** 18);
-    // Participant user = waitlist[msg.sender];
-
-    // if (!user.verified) {
-    //   LogEvent(msg.sender, user.startAge, "sender is not verified");
-    //   throw;
-    // }
-
-    // if (user.added) {
-    //   LogEvent(msg.sender, user.startAge, "sender is already participating");
-    //   throw;
-    // } 
-
-    // if(msg.value < COST) {
-    //   LogEvent(msg.sender, user.startAge, "did not meet buy-in criteria");
-    //   throw;
-    // }
-
-    // if (block.timestamp > user.approvalDate + 1 years) {
-    //   LogEvent(msg.sender, user.startAge, "approval is only valid for one year");
-    //   delete waitlist[msg.sender];
-    //   throw;
-    // }
     
     user.verified = false;
     user.added = true;
@@ -180,7 +148,10 @@ contract Instrument {
   }
   
   /**
-
+   * @dev Returns the index of the correct pool for a given age.
+   * @param age Age of user we are concerned with.
+   * @return idx The index of the pool users of this age are
+   *   allocated to.
    */
   function poolForAge(uint age) public returns (uint idx) {
     uint BASE = 20;
@@ -195,7 +166,10 @@ contract Instrument {
   }
   
   /**
-
+   * @dev A user can invoke this function if they want to exit the 
+   *   contract, leave their pool and get their money back. Can only be
+   *   invoked within 5 years of signing up, and if the pool is not 
+   *   receiving dividends.
    */
   function earlyExit() public stopInEmergency {
     for (var p = 0; p < pools.length; p++) {
@@ -203,7 +177,7 @@ contract Instrument {
         
         // restrictions on early exit
         require(pools[p].midAge < 70);
-        require(block.timestamp < waitlist[msg.sender].approvalDate + 5 years);
+        require(block.timestamp < verifiedUsers[msg.sender].approvalDate + 5 years);
 
         // remove from pool
         IterableMapping.remove(pools[p].participants, msg.sender);
@@ -221,8 +195,9 @@ contract Instrument {
   }
   
   /**
-   Admin passes the contract a list of people to withdraw
-   from program. The program will loop through 
+   * @dev Admin passes a list of people who failed to pass verifying 
+   *   genetic info to the org. Will remove those users from
+   * @param user The cleared user to be added.
    */
   function removeFromPool(address[] addrs) public {
     uint count = 0;
@@ -268,15 +243,7 @@ contract Instrument {
           pendingDividends[msg.sender] = amount;
           return false;
         }
-
-        // for (var p = 0; p < pools.length; p++) {
-        //   if (IterableMapping.contains(pools[p].participants, addrs[i])) {
-        //     pools[p].totalEth -= amount;
-        //     break;
-        //   }
-        // }
       }
-
       return true;
   }
   
