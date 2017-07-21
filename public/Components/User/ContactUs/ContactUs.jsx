d36b2a7695c9bc78653cf5e1aaeb72f91f5309b5
William Wang <will.wang8421@gmail.comgit config --global user.email will.wang8421@gmail.com>
2017-07-21 12:06:31 -0700

    added api call to api etherscan

--- public/Components/User/ContactUs/ContactUs.jsx
+++ public/Components/User/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './contactus.css';
+import axios from 'axios';
 
 // const ContactUs = () => (
 //   <div className="bg10">
@@ -20,6 +21,31 @@ import './contactus.css';
 // )
 
 class ContactUs extends Component {
+  constructor(){
+    super();
+    this.state = {
+      ethPrice: null
+    }
+  }
+
+
+  componentDidMount() {
+    const addCommas = str => {
+      return (str + "").replace(/\b(\d+)((\.\d+)*)\b/g, function(a, b, c) {
+        return (b.charAt(0) > 0 && !(c || ".").lastIndexOf(".") ? b.replace(/(\d)(?=(\d{3})+$)/g, "$1,") : b) + c;
+        });
+      }
+    axios.get("https://api.etherscan.io/api?module=account&action=balance&address=0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe&tag=latest&apikey=IUKZ7J9R48Z4EZAE6XG6VGAYIWU36RPZYQ")
+      .then(price => {
+        console.log('this is the price', addCommas(price.data.result / Math.pow(10,18)));
+        let priceOfEth = addCommas(price.data.result / Math.pow(10,18));
+        this.setState({ethPrice: priceOfEth});
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
   render(){
     return(
   <div className="container">
@@ -38,6 +64,30 @@ class ContactUs extends Component {
           <button type="submit" className="submit">Submit</button>
         </form>
     </section>
+      <div className="donation">
+        <br />
+        <br />
+          <h1>Donate to support Development</h1>
+          <br/>
+            <p><b>Gennuity's mission</b> is to provide individual's income and help accumulate assests during retirement.</p>
+            <p></p>
+            <p></p>
+            <p>All funds received and any proposal to spend it are visible on the blockchain for anyone to see.</p>
+            <p>The process is fully transparent and governed by fraud-proof rules programmed on the blockchain, </p>
+            <p>so you'll know your donations will be well taken care of.</p>
+            <br/>
+            <br/>
+            <br/>
+            <div>
+              <h3>Donation Address</h3>
+              <br/>
+              <h5>0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359</h5>
+              <br/>
+            </div>
+            <div>
+              <h6>Amount Received: {this.state.ethPrice} Eth </h6>
+            </div>
+      </div>
   </div>
     )
   }
