406edee18ed6dcecf6294159380e17c54451f375
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-07-25 08:19:18 -0700

    Tried to fix some of the documentation, but there are files missing

--- public/Components/SmartContract/SmartContract.jsx
+++ public/Components/SmartContract/SmartContract.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
-import htmlFile from '../SmartContract/code-prettify/styles/index.html';
+
+// this is the wrong html, is missing in repo
+import htmlFile from '../../bower_components/google-code-prettify/styles/index.html';
+
 import './smartContract.css';
-import '../SmartContract/code-prettify/styles/sunburst.css';
+import '../../bower_components/google-code-prettify/styles/sunburst.css';
 import sunburst from './sunburst.js';
-import $ from 'jquery';
 
 class SmartContract extends Component {
   constructor(props) {
