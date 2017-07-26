205e163cb4fc67bd1f62bce2312884108a646f4c
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-07-26 12:20:17 -0700

    Graph css changes

--- public/Components/Stats/Graph.jsx
+++ public/Components/Stats/Graph.jsx
@@ -4,8 +4,8 @@ import roi from './data/roi.js';
 
 const SimpleAreaChart = (props) => (
   <div id="graph">
-    <ResponsiveContainer width="100%" minWidth={100} minHeight={100} aspect={1.3}>
-      <AreaChart width={600} height={500} data={roi}
+    <ResponsiveContainer width="100%" minWidth={50} minHeight={50} aspect={1.3}>
+      <AreaChart width={200} height={150} data={roi}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
         <defs>
           <linearGradient id="colorROI" x1="0" y1="0" x2="0" y2="1">
